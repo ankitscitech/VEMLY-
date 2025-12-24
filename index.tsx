73f5cf0ddddf9a48392423188538c2d2385fceba
ankitscitech <ankit.scitech@gmail.com>
2025-12-24 21:22:16 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';   // 🔥 THIS WAS MISSING
+import './index.css';
 
 const rootElement = document.getElementById('root');
+
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  document.body.innerHTML = 'ROOT ELEMENT NOT FOUND';
+  throw new Error('Could not find root element');
 }
 
 const root = ReactDOM.createRoot(rootElement);
+
 root.render(
   <React.StrictMode>
     <App />
